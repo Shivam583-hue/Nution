d169c87db1f8d8aa0ef1cbae038405d25276095c
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-19 13:57:34 +0600

    sidebar scafolded

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DocumnetsPage = () => {
+  return (
+    <div>DocumnetsPage</div>
+  )
+}
+
+export default DocumnetsPage
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { Spinner } from "@/components/spinner"
 import { Button } from "@/components/ui/button"
+import { SignInButton } from "@clerk/nextjs"
+import { useConvexAuth } from "convex/react"
 import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 
 const Heading = () => {
+
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +19,27 @@ const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Nution is the connected workspace where <br /> better&#44; faster work happens&#46;
       </h3>
-      <Button>
-        Enter Nution
-        <ArrowRight className="ml-2 h-4 w-4" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Nution
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton>
+          <Button >
+            Get Nution Free
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
